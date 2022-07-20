3f70ed6233346a7e3a944d90bde9c143db0ef1f1
colklimacisi <75450706+colklimacisi@users.noreply.github.com>
2022-07-20 17:59:57 +0300

    camerafolowscripti
    camerafolowscripti yazıldı

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    public Transform leftBounds;
+    public Transform rightBounds;
+    private float camWidth, camHeight, levelMinX, levelMaxX;
+    public float smoothDampTime = 0.15f;
+        // Start is called before the first frame update
     void Start()
     {
+        camHeight = Camera.main.orthographicSize = 2;
+        camWidth = camHeight + Camera.main.aspect;
+        float leftBoundsWidth = leftBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x/2;
+        float rightBoundsWidth = rightBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+        levelMinX = leftBounds.position.x + leftBoundsWidth + (camWidth / 2);
+        levelMaxX = rightBounds.position.x - rightBoundsWidth - (camWidth / 2);
         
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (target) { float targetx = Mathf.Max(levelMinX, Mathf.Min(levelMaxX, target.position.x));
+        float x= Mathf.SmoothDamp()
+        }
         
     }
 }
