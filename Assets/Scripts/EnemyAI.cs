7fa05c01a4e2e5db539124987ce079761de1d290
colklimacisi <75450706+colklimacisi@users.noreply.github.com>
2022-08-06 22:12:26 +0300

    bug fix

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -54,9 +54,10 @@ public class EnemyAI : MonoBehaviour
             if (velocity.y<=0)
             {
                 pos = CheckGround(pos);
-                
+                CheckWalls(pos, scale.x);
+
             }
-            CheckWalls(pos, scale.x);
+            
             transform.localPosition = pos;
             transform.localScale = scale;
 
@@ -106,9 +107,9 @@ public class EnemyAI : MonoBehaviour
     }
     void CheckWalls(Vector3 pos,float direction)
     {
-        Vector2 originTop = new Vector2(pos.x + direction*0.4f , pos.y - 0.5f -0.2f);
+        Vector2 originTop = new Vector2(pos.x + direction*0.4f , pos.y + .5f -0.2f);
         Vector2 originMiddle = new Vector2(pos.x + direction * 0.4f, pos.y);
-        Vector2 originBottom = new Vector2(pos.x + direction * 0.4f, pos.y - 0.5f+0.2f);
+        Vector2 originBottom = new Vector2(pos.x + direction * 0.4f, pos.y - .5f+0.2f);
         RaycastHit2D wallTop = Physics2D.Raycast(originTop, new Vector2(direction,0), velocity.x * Time.deltaTime, wallMask);
         RaycastHit2D wallMiddle = Physics2D.Raycast(originMiddle, new Vector2(direction, 0), velocity.x* Time.deltaTime, wallMask);
         RaycastHit2D wallBottom = Physics2D.Raycast(originBottom, new Vector2(direction, 0), velocity.x * Time.deltaTime, wallMask);
